e1e5a31057da48977584b7328bdf3a2a1db0c5e8
Hao Chen <hche8093@uni.sydney.edu.au>
2022-11-09 23:22:51 +1100

    Add submit guess function

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 
 const useWordle = () => {
-	const [turn, setTurn] = useState(0);
+	const [turn, setTurn] = useState(5);
 	const [currentGuess, setCurrentGuess] = useState("");
 	const [guesses, setGuesses] = useState([]); // each guess is an array
-	const [history, setHistory] = useState([]); // each guess is a string
+	const [history, setHistory] = useState(["hello"]); // each guess is a string
 	const [isCorrect, setIsCorrect] = useState(false);
 
 	// Format a guess into an array
-	const formatGuess = () => {};
+	const formatGuess = () => {
+		console.log("Formatting guess - ", currentGuess);
+	};
 
 	// Add a new guess
 	const addNewGuess = () => {};
@@ -16,6 +18,25 @@ const useWordle = () => {
 	// Track current guess
 	const handleKeyup = ({ key }) => {
 		// console.log(key);
+		if (key === "Enter") {
+			if (turn < 5) {
+				console.log("You have used all your guesses");
+				return;
+			}
+
+			if (history.includes(currentGuess)) {
+				console.log("You already tried that word");
+				return;
+			}
+
+			if (currentGuess.length !== 5) {
+				console.log("World must be 5 chars long");
+				return;
+			}
+
+			formatGuess();
+		}
+
 		if (key === "Backspace") {
 			setCurrentGuess((prev) => {
 				return prev.slice(0, -1);
